9f59d48f3169d15ad114fbdb6b966abcfbbc5419
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-01-18 16:56:09 +0200

    Update operate.test.js

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -6,7 +6,22 @@ describe("operate function check", () => {
     const sum = operate(10, 5, "+");
     expect(sum).toBe("15");
   });
-
+test('sub', () => {
+    const sum = operate(10, 5, '-');
+    expect(sum).toBe('5');
+  });
+  test('mul', () => {
+    const sum = operate(10, 5, 'x');
+    expect(sum).toBe('50');
+  });
+  test('div', () => {
+    const sum = operate(10, 5, '÷');
+    expect(sum).toBe('2');
+  });
+  test('mod', () => {
+    const sum = operate(10, 5, '%');
+    expect(sum).toBe('0');
+  });
   test("divide by 0", () => {
     const sum = operate(10, 0, "%");
     expect(sum).toBe("Can't find modulo as can't divide by 0.");
